e251e1955c78aa0e73b0f461c5529929a6f12321
fox-yokai <pat.w.cain@gmail.com>
2020-05-01 20:40:31 -0500

    Updates

---
+++ precache-manifest.719a2ddff6917c4bcb267bf64d27a978.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94cc8361aa5c7cc61f8b6d6f8ec4bb67",
+    "url": "/fox-yokai/employee/index.html"
+  },
+  {
+    "revision": "f8a682a3d805234fce68",
+    "url": "/fox-yokai/employee/static/css/main.3fcd11d6.chunk.css"
+  },
+  {
+    "revision": "aad3f21f3cbf0fe5ef6c",
+    "url": "/fox-yokai/employee/static/js/2.021e1ef6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fox-yokai/employee/static/js/2.021e1ef6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8a682a3d805234fce68",
+    "url": "/fox-yokai/employee/static/js/main.1b563269.chunk.js"
+  },
+  {
+    "revision": "c2f3663e6a93d627001f",
+    "url": "/fox-yokai/employee/static/js/runtime-main.9bc8f37d.js"
+  }
+]);
